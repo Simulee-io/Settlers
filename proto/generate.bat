17280ed3cffbc26a7ca24283df2b97464c6142af
Adam Isenberg <adam.isenberg@advance.io>
2021-11-24 19:22:56 +0200

    changed generated go.pb path : part 2

--- proto/generate.bat
+++ proto/generate.bat
@@ -1,2 +1,2 @@
-protoc --go_out=../server/proto/ --go_opt=paths=source_relative board.proto
-protoc --go_opt=paths=source_relative --go_out=plugins=grpc:../server/proto/ settlers.proto
+protoc --go_out=../server/proto/board/ --go_opt=paths=source_relative board.proto
+protoc --go_opt=paths=source_relative --go_out=plugins=grpc:../server/proto/service/ settlers.proto
