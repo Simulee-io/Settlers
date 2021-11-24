638cc524ae1bad66bb531e5eb463aa0978ceb121
Adam Isenberg <adam.isenberg@advance.io>
2021-11-24 20:36:31 +0200

    added client call for diceroll function in pseudo client

--- pseudo_client/client.go
+++ pseudo_client/client.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
 	"google.golang.org/grpc"
-	settlers "simiulee.io/settlers/server/service"
+	settlers "simiulee.io/settlers/server/proto/service"
 )
 
 func main() {
 	fmt.Println("pseudo client initialized")
 
-	cc, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	cc, err := grpc.Dial("0.tcp.ngrok.io:19904", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("could not connect: %v", err)
 	}
@@ -20,6 +21,20 @@ func main() {
 
 	c := settlers.NewSettlersClient(cc)
 
-	fmt.Printf("Created client %f", c)
+	//fmt.Printf("Created client %f", c)
 
+	doUnary(c)
+
+}
+
+func doUnary(c settlers.SettlersClient) {
+	req := &settlers.DiceRequest{}
+	resp, err := c.RollDice(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling dice roll: %v", err)
+	}
+
+	log.Println("Response from dice roll:")
+	log.Printf("dice 1: %v", resp.Dice1)
+	log.Printf("dice 2: %v", resp.Dice2)
 }
