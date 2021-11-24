7bc03fb239ab48e5afdb9d8c157f189957d1ad5f
Adam Isenberg <adam.isenberg@advance.io>
2021-11-24 19:53:01 +0200

    added basic dice roll function to server

--- server/server.go
+++ server/server.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 
 	"google.golang.org/grpc"
-	settlers "simiulee.io/settlers/server/service"
+	settlers "simiulee.io/settlers/server/proto/service"
 )
 
 type server struct{}
 
-func (s server) mustEmbedUnimplementedSettlersServer() {
-	panic("implement me")
+func (*server) RollDice(ctx context.Context, in *settlers.DiceRequest) (*settlers.DiceResponse, error) {
+	d1 := (rand.Intn(6) + 1)
+	d2 := (rand.Intn(6) + 1)
+
+	response := &settlers.DiceResponse{
+		Dice1: int32(d1),
+		Dice2: int32(d2),
+	}
+
+	return response, nil
 }
 
 func main() {
@@ -29,4 +39,5 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v, err")
 	}
+
 }
